d4ed6d6d49a16af82d359038c88b59a9b39be45a
duybluemind1988 <bluemind1988@gmail.com>
2020-11-26 20:50:20 +0700

    add git token

--- ML_project.R
+++ ML_project.R
@@ -9,3 +9,11 @@ murders %>% ggplot() +
   geom_point(aes(x = population/10^6, y = total))
 
 p + geom_point(aes(population/10^6, total))
+
+install.packages('gitcreds')
+library(gitcreds)
+gitcreds_set()
+gitcreds_get()
+install.packages('credentials')
+library(credentials)
+set_github_pat()
